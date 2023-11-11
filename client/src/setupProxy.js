984632eeec64ca05040677f6041d793fbb972cb6
Alviandocendrasa <alviandocendrasa@gmail.com>
2023-11-11 14:09:51 +0800

    update proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     ["/api/*"],
     createProxyMiddleware({
-      target: process.env.NODE_ENV === "development" ? "http://localhost:3140" : "https://csit-314-production.up.railway.app",
+      target: "https://csit-314-production.up.railway.app",
     })
   );
 };
